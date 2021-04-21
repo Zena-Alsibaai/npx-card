8cfaa90e944567ed4d041e12e012f9df1157b033
Zena-Alsibaai <zena.alsibaai@gmail.com>
2021-04-21 14:04:42 +0200

    Change the json name

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # npx-card
+
+This is a solo project to learn how to create a npm package et publish it on [npm](https://www.npmjs.com)
+
+## Where ?
+
+This is a challenge assigned by [BECODE](https://becode.org/) training center, to learning how to code npx business card on npm
+
+## Usage :
+
+Via npx :
+npx
+
+Voilà ;)
---
+++ index.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+const chalk = require("chalk"); // package for colors
+const boxen = require("boxen");
+
+// Text + chalk definitions
+const data = {
+	name: chalk.white("             Zena Alsibaai"),
+	handle: chalk.white("Zena"),
+	npm: chalk.gray("https://www.npmjs.com/") + chalk.red("~zena.alsibaai"),
+	github: chalk.gray("https://github.com/") + chalk.green("Zena-Alsibaai"),
+	linkedin:
+		chalk.gray("https://www.linkedin.com/in/") + chalk.blue("zena-alsibaai/"),
+	web: chalk.cyan("https://zena-alsibaai.github.io/Semaine-07/"),
+	npx: chalk.red("npx") + " " + chalk.white("Zena"),
+	labelNpm: chalk.white.bold("        npm:"),
+	labelGitHub: chalk.white.bold("     GitHub:"),
+	labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+	labelWeb: chalk.white.bold("        Web:"),
+	labelCard: chalk.white.bold("       Card:"),
+};
+
+const NEWLINE = "\n";
+const EMPTYLINE = "";
+
+console.log(
+	chalk.green(
+		boxen(
+			[
+				`${data.name} / ${data.handle}`,
+				EMPTYLINE,
+				`${data.labelNpm} ${data.npm}`,
+				`${data.labelGitHub} ${data.github}`,
+				`${data.labelLinkedIn} ${data.linkedin}`,
+				`${data.labelWeb} ${data.web}`,
+				EMPTYLINE,
+				`${data.labelCard} ${data.npx}`,
+			].join(NEWLINE),
+			{
+				padding: 1,
+				margin: 1,
+				borderStyle: "round",
+			}
+		)
+	)
+);
